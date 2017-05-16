2e2e8999170863f97df45c9d2fae77ecfd4218c0
Simone <bubbleseal@gmail.com>
2017-05-16 08:36:07 +0100

    Implemented Parceable for Item class

--- app/src/main/java/com/example/seal/dairy2u/Item.java
+++ app/src/main/java/com/example/seal/dairy2u/Item.java
@@ -1,6 +1,9 @@
 package com.example.seal.dairy2u;
 
-public class Item {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Item implements Parcelable{
 
     public String name;
     public String price;
@@ -13,5 +16,37 @@ public class Item {
         this.name = name;
         this.price = price;
         this.type = type;
+    }
+
+    public Item(Parcel in) {
+        super();
+        readFromParcel(in);
+    }
+
+    public static final Parcelable.Creator<Item> CREATOR = new Parcelable.Creator<Item>() {
+        public Item createFromParcel(Parcel in) {
+            return new Item(in);
+        }
+
+        public Item[] newArray(int size) {
+
+            return new Item[size];
+        }
+
+    };
+
+    public void readFromParcel(Parcel in) {
+        this.name = in.readString();
+        this.price = in.readString();
+        this.type = in.readString();
+    }
+    public int describeContents() {
+        return 0;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(price);
+        dest.writeString(type);
     }
 }
